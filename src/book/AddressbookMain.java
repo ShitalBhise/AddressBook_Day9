c73f36f4abac12b20426f807c1c32cdc974f322a
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-24 19:24:44 +0530

    Added: Logic for editing the person

--- src/book/AddressbookMain.java
+++ src/book/AddressbookMain.java
@@ -1,20 +1,14 @@
 package book;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class AddressbookMain {
 
 	static Scanner scanner = new Scanner(System.in);
-	ArrayList<AddressBook> person = new ArrayList<AddressBook>();
+	LinkedList<AddressBook> person = new LinkedList<>();
 
-	// constructor
-	public AddressbookMain() {
-	}
-
-	// new person added to record in array list after taking input
-
-	public AddressBook addPerson() {
+	public void addPerson() {
 		System.out.println("Enter the First Name");
 		String fName = scanner.next();
 		System.out.println("Enter the Last Name");
@@ -31,24 +25,74 @@ public class AddressbookMain {
 		String phoneNumber = scanner.next();
 		System.out.println("Enter the Email");
 		String email = scanner.next();
-		// creating constructor
-		AddressBook addressBook = new AddressBook(fName, lName, address, city, state, zip, phoneNumber, email);
+//creating Addressbook object and passing arguments inside constructor
+		AddressBook p = new AddressBook(fName, lName, address, city, state, zip, phoneNumber, email);
+//adding p object into the linked list with Addressbook class data type
+		person.add(p);
+//printing person array list
+		System.out.println(person);
 
-		// add the address book
-		person.add(addressBook);
-
-		// printing AddressBook object data inside
-		System.out.println(addressBook);
-
-		return addressBook;
-
-	}// end of add person method
+	}
 
 	// Edit AddressBook details using person name
+
+	/// editing contact by searching names
+	public void editPerson() {
+		System.out.println("Enter the name to edit");
+		String s = scanner.next();
+
+		for (int i = 0; i < person.size(); i++) {
+			AddressBook p = person.get(i);
+			if (s.equals(p.getfname())) {
+				while (true) {
+					System.out.println("Enter choice to edit 1)firstName\n2)lastName\n3)address\n"
+							+ "4)city\n5)state\n6)zip\n7)phoneNumber\n8)email9)exit");
+					int choice = scanner.nextInt();
+					switch (choice) {
+					case 1:
+						p.setfname(scanner.next());
+						break;
+					case 2:
+						p.setlname(scanner.next());
+						break;
+					case 3:
+						p.setaddress(scanner.next());
+						break;
+					case 4:
+						p.setcity(scanner.next());
+						break;
+					case 5:
+						p.setstate(scanner.next());
+						break;
+					case 6:
+						p.setzip(scanner.next());
+						break;
+					case 7:
+						p.setphoneNo(scanner.next());
+						break;
+					case 8:
+						p.setemail(scanner.next());
+						break;
+					default:
+						System.out.println("select correct choice");
+						break;
+					}// end of switch
+					if (choice == 9)
+						break;
+				} // end while
+				person.set(i, p);
+				System.out.println("person after editing");
+				System.out.println(person);
+
+			} // end of if
+		} // end of for loop
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program in AddressBookMain class");
 		AddressbookMain xy = new AddressbookMain();
 		xy.addPerson();
+		xy.editPerson();
 	}
 }
